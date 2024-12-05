dc8328ea8c686285cd9b33bcff790d606f42c82d
Ahmed helmy <124277597+HelmiiicAcid23@users.noreply.github.com>
2024-12-06 00:33:12 +0200

    Add files via upload
    first done

--- main.ts
+++ main.ts
@@ -1,17 +1,14 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import dbConnection from "./src/config/database";
-import catagoriesRouter from "./src/catagories/catagories.Route";
-import SubcatagoriesRouter from "./src/SubCatagories/SubCatagories.route";
+import mountRoutes from "./src";
 
 const app: express.Application = express();
 
 app.use(express.json({limit: "10kb"}));
 dotenv.config()
 dbConnection()
-app.use('/api/v1/catagories', catagoriesRouter);
-app.use('/api/v1/subcatagories', SubcatagoriesRouter);
-
+mountRoutes(app)
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`)
 })
